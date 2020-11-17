3415cb85aa332251157a1ce63c1647a4c98979e3
yevheniikoliesnik <yevhenii.ko@ideo-digital.com>
2020-11-17 13:33:15 +0200

    add helper project

---
+++ Expenditure.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Expenditure.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ IntegrationHelper/SmtpChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationHelper
+{
+    public static class SmtpChecker
+    {
+        public static void CheckSmtp()
+        {
+            try
+            {
+                var client = new SmtpClient("smtp.office365.com", 587);
+
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("doors@tuboul.co.il", "Digita1");
+                client.EnableSsl = true;
+
+                client.Send("doors@tuboul.co.il", "yevhenii.ko@ideo-digital.com", "test", "testbody");
+
+                Console.WriteLine("Sent");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
